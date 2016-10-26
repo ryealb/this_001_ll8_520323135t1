ea22bd16e3825c9dda6cde67f35ffb7a1b1411b9
rye <ryealb@gmail.com>
2016-10-26 16:30:03 -0400

    things

--- app/src/main/java/com/thingsandsuch/wulz/MainActivity.java
+++ app/src/main/java/com/thingsandsuch/wulz/MainActivity.java
@@ -1,8 +1,13 @@
 package com.thingsandsuch.wulz;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,10 +20,32 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void do_the_thing() {
+    public void do_the_thing(View view) {
+        EditText text = (EditText) findViewById(R.id.led_user_name);
+        String text_value = text.getText().toString();
 
-        Toast.makeText(getApplicationContext(), "this this this", Toast.LENGTH_LONG).show();
-    }
+        EditText password = (EditText) findViewById(R.id.led_password);
+        String password_value = password.getText().toString();
+
+//        ConnectivityManager connMgr = (ConnectivityManager)
+//                getSystemService(Context.CONNECTIVITY_SERVICE);
+//        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+//        if (networkInfo != null && networkInfo.isConnected()) {
+        Toast.makeText(getApplicationContext(), "this this this "+text_value +password_value, Toast.LENGTH_LONG).show();
+
+//        } else {
+//            Toast.makeText(getApplicationContext(), "no internets", Toast.LENGTH_LONG).show();
+
+
+        }
+
+        // login to reddit
+
+
+
+
+
+//    }
 
 
 }
