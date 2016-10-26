d51e9c2e6b97a18bef633c01d9672dc6647cd5f5
ryealb <ryealb@gmail.com>
2016-10-25 23:16:47 -0400

    button

--- app/src/main/java/com/thingsandsuch/wulz/MainActivity.java
+++ app/src/main/java/com/thingsandsuch/wulz/MainActivity.java
@@ -2,6 +2,8 @@ package com.thingsandsuch.wulz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,8 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
+    public void do_the_thing() {
+
+        Toast.makeText(getApplicationContext(), "this this this", Toast.LENGTH_LONG).show();
+    }
 
 
 }
